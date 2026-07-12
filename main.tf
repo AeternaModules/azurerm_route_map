c4a97c5389b8a1c8ddcb01c0a99ed0a8c30f8b3a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 13:20:44 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "azurerm_route_map" "route_maps" {
   virtual_hub_id = each.value.virtual_hub_id
 
   dynamic "rule" {
-    for_each = each.value.rule != null ? [each.value.rule] : []
+    for_each = each.value.rule != null ? each.value.rule : []
     content {
       dynamic "action" {
-        for_each = rule.value.action != null ? [rule.value.action] : []
+        for_each = rule.value.action != null ? rule.value.action : []
         content {
           dynamic "parameter" {
-            for_each = action.value.parameter != null ? [action.value.parameter] : []
+            for_each = action.value.parameter != null ? action.value.parameter : []
             content {
               as_path      = parameter.value.as_path
               community    = parameter.value.community
@@ -22,7 +22,7 @@ resource "azurerm_route_map" "route_maps" {
         }
       }
       dynamic "match_criterion" {
-        for_each = rule.value.match_criterion != null ? [rule.value.match_criterion] : []
+        for_each = rule.value.match_criterion != null ? rule.value.match_criterion : []
         content {
           as_path         = match_criterion.value.as_path
           community       = match_criterion.value.community
--- variables.tf
+++ variables.tf
@@ -24,24 +24,24 @@ EOT
   type = map(object({
     name           = string
     virtual_hub_id = string
-    rule = optional(object({
-      action = optional(object({
-        parameter = optional(object({
+    rule = optional(list(object({
+      action = optional(list(object({
+        parameter = optional(list(object({
           as_path      = optional(list(string))
           community    = optional(list(string))
           route_prefix = optional(list(string))
-        }))
+        })))
         type = string
-      }))
-      match_criterion = optional(object({
+      })))
+      match_criterion = optional(list(object({
         as_path         = optional(list(string))
         community       = optional(list(string))
         match_condition = string
         route_prefix    = optional(list(string))
-      }))
+      })))
       name                 = string
       next_step_if_matched = optional(string) # Default: "Unknown"
-    }))
+    })))
   }))
 }
 
