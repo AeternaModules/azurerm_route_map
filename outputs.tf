c33fb8af7d450127e1c9fd848b9937d996fe8f09
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "route_maps_id" {
   description = "Map of id values across all route_maps, keyed the same as var.route_maps"
-  value       = { for k, v in azurerm_route_map.route_maps : k => v.id }
+  value       = { for k, v in azurerm_route_map.route_maps : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "route_maps_name" {
   description = "Map of name values across all route_maps, keyed the same as var.route_maps"
-  value       = { for k, v in azurerm_route_map.route_maps : k => v.name }
+  value       = { for k, v in azurerm_route_map.route_maps : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "route_maps_rule" {
   description = "Map of rule values across all route_maps, keyed the same as var.route_maps"
-  value       = { for k, v in azurerm_route_map.route_maps : k => v.rule }
+  value       = { for k, v in azurerm_route_map.route_maps : k => v.rule if v.rule != null && length(v.rule) > 0 }
 }
 output "route_maps_virtual_hub_id" {
   description = "Map of virtual_hub_id values across all route_maps, keyed the same as var.route_maps"
-  value       = { for k, v in azurerm_route_map.route_maps : k => v.virtual_hub_id }
+  value       = { for k, v in azurerm_route_map.route_maps : k => v.virtual_hub_id if v.virtual_hub_id != null && length(v.virtual_hub_id) > 0 }
 }
 
